afdb442d057005a0106cba30eb8c2f3742184161
HopewellITdept <ITdept@hopewellva.gov>
2026-02-24 23:59:37 -0500

    Update EventCalendarEnhanced.tsx

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -189,6 +189,7 @@ const EventCalendarEnhanced = () => {
                     src="/lovable-uploads/Ship-offshore.png"
                     alt="Photo of a ship"
                     className="w-full rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+                    title="City Point Historic District Walking Tour"
                   />
                 </a>
 </td>
