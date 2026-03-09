3b58c0bf2a13d8a7e36e2d748db5962b041ed425
HopewellITdept <ITdept@hopewellva.gov>
2026-03-09 17:03:10 -0400

    Update EventCalendarEnhanced.tsx

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -174,9 +174,9 @@ const EventCalendarEnhanced = () => {
                 transition={{ duration: 0.8, delay: 0.8 }}
               >
 
-<table style="width: 100px; border-collapse: collapse; border: none;">
+<table style={{ width: '100px', borderCollapse: 'collapse', border: 'none' }}>
   <tr>
-<td style="width: 50px; border: none; padding: 5px;">      
+<td style={{ width: 50px; border: 'none', padding: '5px' }}>      
                 <a
                   href="https://storymaps.arcgis.com/stories/ba38d853448343528c775a471a1646ce" 
                   target="_blank" 
@@ -192,7 +192,7 @@ const EventCalendarEnhanced = () => {
                   />
                 </a>
 </td>
-<td style="width: 50px; border: none; padding: 5px;">       
+<td style={{ width: 50px; border: 'none', padding: '5px' }}>        
                 <a
                   href="https://storymaps.arcgis.com/stories/a1095c64e5ac45a59ef6a20a7183d45e" 
                   target="_blank" 
