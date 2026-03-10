8afbe0f29c3468a28bd9f12ce66d4f7f9d338140
HopewellITdept <ITdept@hopewellva.gov>
2026-03-10 12:58:29 -0400

    Update EventCalendarEnhanced.tsx

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -183,13 +183,13 @@ const EventCalendarEnhanced = () => {
                   rel="noopener noreferrer"
                   className="block max-w-xs"
                 >
-                  <img
+                  <figure><img
                     src="/lovable-uploads/Ship-offshore.png"
                     width="125"
                     alt="Photo of a ship"
                     className="w-full rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
                     title="City Point Historic District Walking Tour"
-                  />
+                  /><figcaption>City Point Historic District Walking Tour</figcaption></figure>
                 </a>
 </td>
 <td style={{ width: '250px', border: 'none', padding: '5px' }}>        
@@ -199,13 +199,13 @@ const EventCalendarEnhanced = () => {
                   rel="noopener noreferrer"
                   className="block max-w-xs"
                 >            
-                  <img
+                  <figure><img
                     src="/lovable-uploads/War-1.png"
                     width="125"
                     alt="Photo of war generals "
                     className="w-full rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
                     title="War Stories and Heroes of Hopewell, Virginia"
-                  />
+                  /><figcaption>War Stories and Heroes of Hopewell, Virginia</figcaption></figure>
                 </a>
                 </td>   
       </tr>
